7c361d3e11117711c328671bf0b1b0bab63579e6
arinatrifonova <144946127+arinatrifonova@users.noreply.github.com>
2026-09-16 20:26:49 +0300

    Create 01_revenue_changing.sql

---
+++ sql/01_revenue_changing.sql
@@ -0,0 +1,23 @@
+-- revenue_changing.sql
+-- Дневная выручка, накопительная выручка и изменение выручки относительно предыдущего дня
+WITH daily_revenue AS (
+    SELECT
+        date,
+        SUM(price) AS revenue
+    FROM (SELECT order_id,
+      			     UNNEST(product_ids) AS product_id,
+    		         creation_time::date AS date
+          FROM orders) AS t
+    LEFT JOIN products USING (product_id)
+    WHERE order_id NOT IN (SELECT order_id
+          					       FROM user_actions
+          					       WHERE action = 'cancel_order')
+    GROUP BY date
+)
+
+SELECT
+    date,
+    revenue,
+    SUM(revenue) OVER (ORDER BY date) AS total_revenue,
+    ROUND(100 * (revenue - LAG(revenue, 1) OVER (ORDER BY date))::decimal / LAG(revenue, 1) OVER (), 2) AS revenue_change
+FROM daily_revenue;
