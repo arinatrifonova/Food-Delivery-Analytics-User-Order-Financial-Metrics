a287041066c3d7f8525d6ae6f59e3c3d9726213e
arinatrifonova <144946127+arinatrifonova@users.noreply.github.com>
2026-09-17 12:37:13 +0300

    Update 01_revenue_changing.sql

--- sql/01_revenue_changing.sql
+++ sql/01_revenue_changing.sql
@@ -1,23 +1,33 @@
--- revenue_changing.sql
--- Дневная выручка, накопительная выручка и изменение выручки относительно предыдущего дня
+-- Этот запрос считает дневную выручку, накопительную выручку и изменение выручки относительно предыдущего дня
+
+-- В CTE daily_revenue рассчитываем дневную выручку
+-- UNNEST преобразует массив product_ids в отдельные строки, затем данные объединяются с таблицей Products для получения стоимости товаров
+-- Отмененные заказы исключаются из рассчетов
 WITH daily_revenue AS (
     SELECT
-        date,
-        SUM(price) AS revenue
-    FROM (SELECT order_id,
-      		     UNNEST(product_ids) AS product_id,
-    		     creation_time::date AS date
-          FROM orders) AS t
-    LEFT JOIN products USING (product_id)
-    WHERE order_id NOT IN (SELECT order_id
-          				   FROM user_actions
-          				   WHERE action = 'cancel_order')
+        date,					
+        SUM(price) AS revenue	
+    FROM (											
+		SELECT 
+			order_id,
+			UNNEST(product_ids) AS product_id,		-- Из массива товаров получаем id каждого купленного товара 
+			creation_time::date AS date
+        FROM orders
+	) AS t
+    LEFT JOIN products USING (product_id)			-- Объединяем данные о заказах с таблицей товаров
+    WHERE order_id NOT IN (							-- Исключаем отмененные заказы из рассчетов
+		SELECT order_id 
+		FROM user_actions 
+		WHERE action = 'cancel_order'
+	)
     GROUP BY date
 )
 
 SELECT
-    date,
-    revenue,
-    SUM(revenue) OVER (ORDER BY date) AS total_revenue,
-    ROUND(100 * (revenue - LAG(revenue, 1) OVER (ORDER BY date))::decimal / LAG(revenue, 1) OVER (ORDER BY date), 2) AS revenue_change
-FROM daily_revenue;
+    date,																		-- Конкретный день
+    revenue,																	-- Выручка за конкретный день
+    SUM(revenue) OVER (ORDER BY date) AS total_revenue,							-- Накопительная выручка с начала периода
+    ROUND(100 * (revenue - LAG(revenue, 1) OVER (ORDER BY date))::decimal /		-- Изменение выручки относительно предыдущего дня в процентах
+					LAG(revenue, 1) OVER (), 2) AS revenue_change
+FROM daily_revenue
+ORDER BY date;
